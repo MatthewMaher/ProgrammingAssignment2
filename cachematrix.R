7c47f99f4b82b7c3863c24a7078c00cae5a9d5be
Matthew <matthew.jl.maher\@gmail.com>
2015-02-13 12:09:26 -0500

    Implementation of routines to cache matrix inversion operations

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of routines to provide for optimization of Matrix Inversion operations
+## by caching results of inversions so that subsequent calls will avoid re-calculation
 
-## Write a short comment describing this function
+## Function to create a wrapper construct around a matrix that also contains
+## a location to store the cached inversion result. 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+        i <- NULL
+        set <- function(y) {
+                m <<- y
+                i <<- NULL
+        }
+        get <- function() m
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Routine to return the inversion of the supplied Matrix (which MUST have earlier been 
+## supplied to makeCacheMatrix) and to cache result for return on subsequent calls.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        ## Return a matrix that is the inverse of 'm'
+        i <- m$getinv()
+        if(!is.null(i)) {
+                return(i)
+        }
+        data <- m$get()
+        i <- solve(data, ...)
+        m$setinv(i)
+        i
 }
